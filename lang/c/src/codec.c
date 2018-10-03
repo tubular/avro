5f5687f0cc152bee108c3f3aa4977e2372aabe87
Christian Verkerk <christian@tubularlabs.com>
2018-10-03 18:42:45 +0300

    IMP define MacOS names for byteorder funcs

--- lang/c/src/codec.c
+++ lang/c/src/codec.c
@@ -2,17 +2,17 @@
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0 
+ * The ASF licenses this file to you under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
  * implied.  See the License for the specific language governing
- * permissions and limitations under the License. 
+ * permissions and limitations under the License.
  */
 
 #include <string.h>
@@ -21,6 +21,8 @@
 #  if defined(__APPLE__)
 #    include <libkern/OSByteOrder.h>
 #    define __bswap_32 OSSwapInt32
+#    define htobe32 OSSwapHostToBigInt32
+#    define be32toh OSSwapBigToHostInt32
 #  elif defined(__FreeBSD__)
 #    include <sys/endian.h>
 #    define __bswap_32 bswap32
@@ -35,6 +37,8 @@
 #    if defined(__APPLE__)
 #      include <libkern/OSByteOrder.h>
 #      define __bswap_32 OSSwapInt32
+#      define htobe32 OSSwapHostToBigInt32
+#      define be32toh OSSwapBigToHostInt32
 #    else
 #      include <byteswap.h>
 #    endif
